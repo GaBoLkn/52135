322a69b7d75d1f2c8f674bc7e44c207425800b0f
GSánchez <116281694+GaBoLkn@users.noreply.github.com>
2025-05-13 14:12:27 -0300

    Add files via upload

---
+++ ssl-antlr-calculator/generated/CalculatorListener.js
@@ -0,0 +1,90 @@
+// Generated from c:/Users/Usuario/ssl-antlr-calculator/Calculator.g4 by ANTLR 4.13.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by CalculatorParser.
+export default class CalculatorListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by CalculatorParser#prog.
+	enterProg(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#prog.
+	exitProg(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#formula.
+	enterFormula(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#formula.
+	exitFormula(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#implicacion.
+	enterImplicacion(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#implicacion.
+	exitImplicacion(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#disyuncion.
+	enterDisyuncion(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#disyuncion.
+	exitDisyuncion(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#conjuncion.
+	enterConjuncion(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#conjuncion.
+	exitConjuncion(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#negacion.
+	enterNegacion(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#negacion.
+	exitNegacion(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#primario.
+	enterPrimario(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#primario.
+	exitPrimario(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#variable.
+	enterVariable(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#variable.
+	exitVariable(ctx) {
+	}
+
+
+	// Enter a parse tree produced by CalculatorParser#variable_char.
+	enterVariable_char(ctx) {
+	}
+
+	// Exit a parse tree produced by CalculatorParser#variable_char.
+	exitVariable_char(ctx) {
+	}
+
+
+
+}
