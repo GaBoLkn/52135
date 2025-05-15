5559b045ab913836fa541c916a0d9fbf44a68d8e
GSánchez <gabrielsanchez040506@gmail.com>
2025-05-15 18:01:42 -0300

    Segundo Commit

---
+++ ssl-antlr-analizador/CustomCalculatorVisitor.js
@@ -0,0 +1,75 @@
+//import { Parser } from "antlr4";
+//import CalculatorVisitor from "./generated/CalculatorVisitor.js";
+//import CalculatorParser from "./generated/CalculatorParser.js";
+
+import { CharStreams, CommonTokenStream } from "antlr4";
+import CalculatorVisitor from "./generated/CalculatorVisitor.js";
+import CalculatorParser from "./generated/CalculatorParser.js";
+import CalculatorLexer from "./generated/CalculatorLexer.js";
+
+export class CustomCalculatorVisitor extends CalculatorVisitor{
+
+    constructor() {
+        super();
+        this.memory = new Map();   //Declaro una variable de instancia con una memoria temporal para hacer las reducciones
+    } 
+    
+    visitProg(ctx) {
+        return ctx.formula().map(f => this.visit(f)).join("\n");
+    }
+
+    visitFormula(ctx) {
+        return this.visit(ctx.implicacion());
+    }
+
+    visitImplicacion(ctx) {
+        if (ctx.implicacion()) {
+            // CORREGIDO: uso de template string correctamente
+            return `(!(${this.visit(ctx.disyuncion())}) || ${this.visit(ctx.implicacion())})`;
+        } else {
+            return this.visit(ctx.disyuncion());
+        }
+    }
+
+    visitDisyuncion(ctx) {
+        const partes = ctx.conjuncion().map(c => this.visit(c));
+        return partes.join(" || ");
+    }
+
+    visitConjuncion(ctx) {
+        const partes = ctx.negacion().map(n => this.visit(n));
+        return partes.join(" && ");
+    }
+
+    visitNegacion(ctx) {
+        if (ctx.NEGATION()) {
+            // CORREGIDO: uso de template string
+            return `!(${this.visit(ctx.negacion())})`;
+        } else {
+            return this.visit(ctx.primario());
+        }
+    }
+
+    visitPrimario(ctx) {
+        if (ctx.variable()) {
+            return this.visit(ctx.variable());
+        } else {
+            // CORREGIDO: uso de template string
+            return `(${this.visit(ctx.formula())})`;
+        }
+    }
+
+    visitVariable(ctx) {
+        return ctx.getText();
+    }
+
+    traducir(input) {
+    const inputStream = CharStreams.fromString(input);
+    const lexer = new CalculatorLexer(inputStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new CalculatorParser(tokenStream);
+    const tree = parser.prog();
+
+    return this.visit(tree); // Usamos this, no TraductorVisitor
+}
+}
---
+++ ssl-antlr-analizador/generated/CalculatorVisitor.js
@@ -0,0 +1,64 @@
+// Generated from c:/Users/Usuario/ssl-antlr-calculator/Calculator.g4 by ANTLR 4.13.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by CalculatorParser.
+
+export default class CalculatorVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by CalculatorParser#prog.
+	visitProg(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#formula.
+	visitFormula(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#implicacion.
+	visitImplicacion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#disyuncion.
+	visitDisyuncion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#conjuncion.
+	visitConjuncion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#negacion.
+	visitNegacion(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#primario.
+	visitPrimario(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#variable.
+	visitVariable(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by CalculatorParser#variable_char.
+	visitVariable_char(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
